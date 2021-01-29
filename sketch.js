cb1d11a81a8f0b3c3ccd9750e50be427299dfdf0
AakritiSrivastava4 <71749750+AakritiSrivastava4@users.noreply.github.com>
2021-01-29 17:52:47 +0530

    Update sketch.js
    trying 2nd approach

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function preload(){
   fruit3_img = loadImage("images/melon2.png");
   fruit4_img = loadImage("images/orange2.png");
   fruit5_img = loadImage("images/pineapple2.png");
-  fruitGroup = new Group();
+ 
 }
 function setup() {
   createCanvas(1000, 600);
